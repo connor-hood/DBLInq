203982575b74787ad25298509577af8ba6ecb8a5
Connor Hood <connor_hood@yahoo.com>
2021-06-10 13:29:57 -0500

    Problem One solved

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -15,7 +15,7 @@ namespace DatabaseFirstLINQ
         }
         public void RunLINQQueries()
         {
-            //ProblemOne();
+            ProblemOne();
             //ProblemTwo();
             //ProblemThree();
             //ProblemFour();
@@ -40,8 +40,12 @@ namespace DatabaseFirstLINQ
         // <><><><><><><><> R Actions (Read) <><><><><><><><><>
         private void ProblemOne()
         {
-            // Write a LINQ query that returns the number of users in the Users table.
-            // HINT: .ToList().Count
+            //Write a LINQ query that returns the number of users in the Users table.
+            //HINT: .ToList().Count
+            var users = _context.Users.ToList().Count;
+            Console.WriteLine(users);
+            //Console.ReadLine();
+                
 
         }
 
